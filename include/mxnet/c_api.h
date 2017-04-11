f64debf058c9afbb7eba51c395602f013e75a8cd
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-04-11 00:35:50 +0000

    Change NDArray constructor to take multiple aux_data

--- include/mxnet/c_api.h
+++ include/mxnet/c_api.h
@@ -218,7 +218,8 @@ MXNET_DLL int MXNDArrayCreate(const mx_uint *shape,
                               NDArrayHandle *out);
 
 MXNET_DLL int MXNDArrayCreateSparse(NDArrayHandle data,
-                    NDArrayHandle aux_data,
+                    int num_aux,
+                    NDArrayHandle *aux_data,
                     const mx_uint *shape,
                     mx_uint ndim,
                     int sparse_type,
@@ -255,7 +256,8 @@ MXNET_DLL int MXNDArrayCreateSparseEx(int chunk_type,
                     int dev_id,
                     int delay_alloc,
                     int dtype,
-                    int aux_type,
+                    int num_aux,
+                    int *aux_types,
                     NDArrayHandle *out);
 
 MXNET_DLL int MXNDArrayConvert(NDArrayHandle in,
