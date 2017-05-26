561ed07f21c4a0b3b94af50ad521aac31037447f
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-05-26 05:39:34 +0000

    also read label from libsvm

--- src/io/iter_libsvm.cc
+++ src/io/iter_libsvm.cc
@@ -31,7 +31,7 @@ struct LibSVMIterParam : public dmlc::Parameter<LibSVMIterParam> {
         .describe("The shape of one example.");
     DMLC_DECLARE_FIELD(label_libsvm).set_default("NULL")
         .describe("The input LibSVM file or a directory path. "
-                  "If NULL, all labels will be returned as 0.");
+                  "If NULL, all labels will be read from ``data_libsvm``.");
     index_t shape1[] = {1};
     DMLC_DECLARE_FIELD(label_shape).set_default(TShape(shape1, shape1 + 1))
         .describe("The shape of one label.");
@@ -46,19 +46,23 @@ class LibSVMIter: public SparseIIterator<DataInst> {
   // intialize iterator loads data in
   virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) {
     param_.InitAllowUnknown(kwargs);
-    data_parser_.reset(dmlc::Parser<uint32_t>::Create(param_.data_libsvm.c_str(), 0, 1, "libsvm"));
+    data_parser_.reset(dmlc::Parser<uint32_t>::Create(param_.data_libsvm.c_str(),
+                                                      0, 1, "libsvm"));
+    CHECK_EQ(param_.data_shape.ndim(), 1) << "dimension of data_shape is expected to be 1";
     if (param_.label_libsvm != "NULL") {
-      label_parser_.reset(dmlc::Parser<uint32_t>::Create(param_.label_libsvm.c_str(), 0, 1, "libsvm"));
+      label_parser_.reset(dmlc::Parser<uint32_t>::Create(param_.label_libsvm.c_str(),
+                                                         0, 1, "libsvm"));
+      CHECK_GT(param_.label_shape.Size(), 1)
+        << "label_shape is not expected to be (1,) when param_.label_libsvm is set.";
     } else {
-      dummy_label.set_pad(false);
-      dummy_label.Resize(mshadow::Shape1(1));
-      dummy_label = 0.0f;
+      CHECK_EQ(param_.label_shape.Size(), 1)
+        << "label_shape is expected to be (1,) when param_.label_libsvm is NULL";
     }
-    // both data and label are in libsvm format
+    // both data and label are of CSRStorage in libsvm format
     if (param_.label_shape.Size() > 1) {
       out_.data.resize(6);
     } else {
-      // only data is in libsvm format. Label is scalar.
+      // only data is of CSRStorage in libsvm format.
       out_.data.resize(4);
     }
   }
@@ -87,9 +91,9 @@ class LibSVMIter: public SparseIIterator<DataInst> {
     CHECK_LT(data_ptr_, data_size_);
     const auto data_row = data_parser_->Value()[data_ptr_++];
     // data, indices and indptr
-    out_.data[0] = AsDataTBlob(data_row);
-    out_.data[1] = AsIdxTBlob(data_row);
-    out_.data[2] = AsIndPtrTBlobPlaceholder(data_row);
+    out_.data[0] = AsDataBlob(data_row);
+    out_.data[1] = AsIdxBlob(data_row);
+    out_.data[2] = AsIndPtrPlaceholder(data_row);
 
     if (label_parser_.get() != nullptr) {
       while (label_ptr_ >= label_size_) {
@@ -101,11 +105,11 @@ class LibSVMIter: public SparseIIterator<DataInst> {
       CHECK_LT(label_ptr_, label_size_);
       const auto label_row = label_parser_->Value()[label_ptr_++];
       // data, indices and indptr
-      out_.data[3] = AsDataTBlob(label_row);
-      out_.data[4] = AsIdxTBlob(label_row);
-      out_.data[5] = AsIndPtrTBlobPlaceholder(label_row);
+      out_.data[3] = AsDataBlob(label_row);
+      out_.data[4] = AsIdxBlob(label_row);
+      out_.data[5] = AsIndPtrPlaceholder(label_row);
     } else {
-      out_.data[3] = dummy_label;
+      out_.data[3] = AsScalarLabelBlob(data_row);
     }
     return true;
   }
@@ -126,20 +130,25 @@ class LibSVMIter: public SparseIIterator<DataInst> {
 
  private:
 
-  inline TBlob AsDataTBlob(const dmlc::Row<uint32_t>& row) {
+  inline TBlob AsDataBlob(const dmlc::Row<uint32_t>& row) {
     const real_t* ptr = row.value;
     TShape shape(mshadow::Shape1(row.length));
-    return TBlob((real_t*)ptr, shape, cpu::kDevMask);
+    return TBlob((real_t*) ptr, shape, cpu::kDevMask);
   }
 
-  inline TBlob AsIdxTBlob(const dmlc::Row<uint32_t>& row) {
+  inline TBlob AsIdxBlob(const dmlc::Row<uint32_t>& row) {
     const uint32_t* ptr = row.index;
     TShape shape(mshadow::Shape1(row.length));
-    return TBlob((int32_t*)ptr, shape, cpu::kDevMask, CSR_IDX_DTYPE);
+    return TBlob((int32_t*) ptr, shape, cpu::kDevMask, CSR_IDX_DTYPE);
   }
 
-  inline TBlob AsIndPtrTBlobPlaceholder(const dmlc::Row<uint32_t>& row) {
+  inline TBlob AsIndPtrPlaceholder(const dmlc::Row<uint32_t>& row) {
     return TBlob(nullptr, mshadow::Shape1(0), cpu::kDevMask, CSR_IND_PTR_TYPE);
+  }
+
+  inline TBlob AsScalarLabelBlob(const dmlc::Row<uint32_t>& row) {
+    const real_t* ptr = row.label;
+    return TBlob((real_t*) ptr, mshadow::Shape1(1), cpu::kDevMask);
   }
 
   LibSVMIterParam param_;
@@ -149,8 +158,6 @@ class LibSVMIter: public SparseIIterator<DataInst> {
   unsigned inst_counter_{0};
   // at end
   bool end_{false};
-  // dummy label
-  mshadow::TensorContainer<cpu, 1, real_t> dummy_label;
   // label parser
   size_t label_ptr_{0}, label_size_{0};
   size_t data_ptr_{0}, data_size_{0};
@@ -163,6 +170,40 @@ DMLC_REGISTER_PARAMETER(LibSVMIterParam);
 
 MXNET_REGISTER_IO_ITER(LibSVMIter)
 .describe(R"code(Returns the LibSVM file iterator.
+
+The input data is in libsvm format. Example input can be downloaded at
+`https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass/shuttle.scale.t`
+
+In this function, the `data_shape` parameter is used to set the shape of each line of the data.
+The dimension of both `data_shape` and `label_shape` are expected to be 1.
+
+When `label_libsvm` is set to ``NULL``, both data and label are read from the same file specified
+by `data_libsvm`. Otherwise, data is read from `data_libsvm` and label from `label_libsvm`,
+in this case, if `data_libsvm` contains label, it will ignored.
+
+The `LibSVMIter` only support `round_batch` parameter set to ``True`` for now. So, if `batch_size`
+is 3 and there are 4 total rows in libsvm file, 2 more examples
+are consumed at the first round. If `reset` function is called after first round,
+the call is ignored and remaining examples are returned in the second round.
+
+If ``data_libsvm = 'data/'`` is set, then all the files in this directory will be read.
+
+Examples::
+
+  // Contents of libsvm file ``data.t``.
+
+  // Creates a `LibSVMIter` with `batch_size`=2.
+  LibSVMIter = mx.io.LibSVMIter(data_libsvm = 'data.t', data_shape = (3,),
+  batch_size = 2)
+
+  // Two batches read from the above iterator are as follows:
+  [[ 1.  2.  3.]
+  [ 2.  3.  4.]]
+  [[ 3.  4.  5.]
+  [ 4.  5.  6.]]
+
+  // TODO(haibin) example for both data and label
+
 )code" ADD_FILELINE)
 .add_arguments(LibSVMIterParam::__FIELDS__())
 .add_arguments(BatchParam::__FIELDS__())
