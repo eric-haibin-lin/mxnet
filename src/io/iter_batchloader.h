03c8bde0dace6c3c28b20a1b8846ad3e083ec1ad
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-05-31 20:51:41 +0000

    revert naming change

--- src/io/iter_batchloader.h
+++ src/io/iter_batchloader.h
@@ -23,7 +23,7 @@ namespace io {
 class BatchLoader : public IIterator<TBlobBatch> {
  public:
   explicit BatchLoader(IIterator<DataInst> *base):
-    head(1), num_overflow(0), base_(base) {
+    head_(1), num_overflow_(0), base_(base) {
   }
 
   virtual ~BatchLoader(void) {
@@ -32,96 +32,96 @@ class BatchLoader : public IIterator<TBlobBatch> {
 
   inline void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) {
     std::vector<std::pair<std::string, std::string> > kwargs_left;
-    // init batch param, it could have similar param with
-    kwargs_left = param.InitAllowUnknown(kwargs);
-    // Init space for out
-    out.inst_index = new unsigned[param.batch_size];
-    out.batch_size = param.batch_size;
-    out.data.clear();
+    // init batch param_, it could have similar param_ with
+    kwargs_left = param_.InitAllowUnknown(kwargs);
+    // Init space for out_
+    out_.inst_index = new unsigned[param_.batch_size];
+    out_.batch_size = param_.batch_size;
+    out_.data.clear();
     // init base iterator
     base_->Init(kwargs);
   }
 
   virtual void BeforeFirst(void) {
-    if (param.round_batch == 0 || num_overflow == 0) {
+    if (param_.round_batch == 0 || num_overflow_ == 0) {
       // otherise, we already called before first
       base_->BeforeFirst();
     } else {
-      num_overflow = 0;
+      num_overflow_ = 0;
     }
-    head = 1;
+    head_ = 1;
   }
 
   virtual bool Next(void) {
-    out.num_batch_padd = 0;
-    out.batch_size = param.batch_size;
-    this->head = 0;
+    out_.num_batch_padd = 0;
+    out_.batch_size = param_.batch_size;
+    this->head_ = 0;
 
     // if overflow from previous round, directly return false, until before first is called
-    if (num_overflow != 0) return false;
+    if (num_overflow_ != 0) return false;
     index_t top = 0;
 
     while (base_->Next()) {
       const DataInst& d = base_->Value();
-      out.inst_index[top] = d.index;
-      if (data.size() == 0) {
+      out_.inst_index[top] = d.index;
+      if (data_.size() == 0) {
         this->InitData(d);
       }
       for (size_t i = 0; i < d.data.size(); ++i) {
         CHECK_EQ(unit_size_[i], d.data[i].Size());
-        MSHADOW_TYPE_SWITCH(data[i].type_flag_, DType, {
+        MSHADOW_TYPE_SWITCH(data_[i].type_flag_, DType, {
             mshadow::Copy(
-              data[i].get<cpu, 1, DType>().Slice(top * unit_size_[i],
+              data_[i].get<cpu, 1, DType>().Slice(top * unit_size_[i],
                                                   (top + 1) * unit_size_[i]),
               d.data[i].get_with_shape<cpu, 1, DType>(mshadow::Shape1(unit_size_[i])));
           });
       }
-      if (++top >= param.batch_size) {
+      if (++top >= param_.batch_size) {
         return true;
       }
     }
     if (top != 0) {
-      if (param.round_batch != 0) {
-        num_overflow = 0;
+      if (param_.round_batch != 0) {
+        num_overflow_ = 0;
         base_->BeforeFirst();
-        for (; top < param.batch_size; ++top, ++num_overflow) {
+        for (; top < param_.batch_size; ++top, ++num_overflow_) {
           CHECK(base_->Next()) << "number of input must be bigger than batch size";
           const DataInst& d = base_->Value();
-          out.inst_index[top] = d.index;
+          out_.inst_index[top] = d.index;
           // copy data
           for (size_t i = 0; i < d.data.size(); ++i) {
             CHECK_EQ(unit_size_[i], d.data[i].Size());
-            MSHADOW_TYPE_SWITCH(data[i].type_flag_, DType, {
+            MSHADOW_TYPE_SWITCH(data_[i].type_flag_, DType, {
                 mshadow::Copy(
-                  data[i].get<cpu, 1, DType>().Slice(top * unit_size_[i],
+                  data_[i].get<cpu, 1, DType>().Slice(top * unit_size_[i],
                                                       (top + 1) * unit_size_[i]),
                   d.data[i].get_with_shape<cpu, 1, DType>(mshadow::Shape1(unit_size_[i])));
               });
           }
         }
-        out.num_batch_padd = num_overflow;
+        out_.num_batch_padd = num_overflow_;
       } else {
-        out.num_batch_padd = param.batch_size - top;
+        out_.num_batch_padd = param_.batch_size - top;
       }
       return true;
     }
     return false;
   }
   virtual const TBlobBatch &Value(void) const {
-    return out;
+    return out_;
   }
 
  protected:
-  /*! \brief batch parameters */
-  BatchParam param;
-  /*! \brief output data */
-  TBlobBatch out;
+  /*! \brief batch param_eters */
+  BatchParam param_;
+  /*! \brief out_put data */
+  TBlobBatch out_;
   /*! \brief on first */
-  int head;
+  int head_;
   /*! \brief number of overflow instances that readed in round_batch mode */
-  int num_overflow;
+  int num_overflow_;
   /*! \brief tensor to hold data */
-  std::vector<TBlobContainer> data;
+  std::vector<TBlobContainer> data_;
 
  private:
   /*! \brief base iterator */
@@ -133,22 +133,22 @@ class BatchLoader : public IIterator<TBlobBatch> {
   // initialize the data holder by using from the first batch.
   inline void InitData(const DataInst& first_batch) {
     shape_.resize(first_batch.data.size());
-    data.resize(first_batch.data.size());
+    data_.resize(first_batch.data.size());
     unit_size_.resize(first_batch.data.size());
     for (size_t i = 0; i < first_batch.data.size(); ++i) {
       TShape src_shape = first_batch.data[i].shape_;
       int src_type_flag = first_batch.data[i].type_flag_;
       // init object attributes
       std::vector<index_t> shape_vec;
-      shape_vec.push_back(param.batch_size);
+      shape_vec.push_back(param_.batch_size);
       for (index_t dim = 0; dim < src_shape.ndim(); ++dim) {
         shape_vec.push_back(src_shape[dim]);
       }
       TShape dst_shape(shape_vec.begin(), shape_vec.end());
       shape_[i] = dst_shape;
-      data[i].resize(mshadow::Shape1(dst_shape.Size()), src_type_flag);
+      data_[i].resize(mshadow::Shape1(dst_shape.Size()), src_type_flag);
       unit_size_[i] = src_shape.Size();
-      out.data.push_back(TBlob(data[i].dptr_, dst_shape, cpu::kDevMask, src_type_flag));
+      out_.data.push_back(TBlob(data_[i].dptr_, dst_shape, cpu::kDevMask, src_type_flag));
     }
   }
 };  // class BatchLoader
