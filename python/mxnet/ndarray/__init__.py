ccd18361460de7483abffc2df177687dc0be1004
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-08-07 19:05:54 +0000

    fix lint

--- python/mxnet/ndarray/__init__.py
+++ python/mxnet/ndarray/__init__.py
@@ -6,5 +6,5 @@ from .op import CachedOp
 # pylint: disable=wildcard-import
 from .ndarray import *
 from .utils import load, save, zeros, empty, array
-from .sparse_ndarray import _ndarray_cls
-from .sparse_ndarray import csr_matrix, row_sparse_array, BaseSparseNDArray, RowSparseNDArray, CSRNDArray
+from .sparse_ndarray import _ndarray_cls, csr_matrix, row_sparse_array
+from .sparse_ndarray import BaseSparseNDArray, RowSparseNDArray, CSRNDArray
