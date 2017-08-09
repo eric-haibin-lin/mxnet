0dc5611086bfac86a63dbfdc80ca5fe39c52c68f
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-08-09 22:57:14 +0000

    Revert "add setitem test to gpu"
    This reverts commit 3b969ac0980e8d7166a1cf46878ed2bd457986ed.

--- tests/python/gpu/test_operator_gpu.py
+++ tests/python/gpu/test_operator_gpu.py
@@ -33,7 +33,7 @@ from test_gluon import *
 from test_gluon_rnn import *
 from test_sparse_operator import test_cast_storage_ex, test_sparse_dot
 from test_sparse_operator import test_sparse_nd_zeros, test_sparse_retain
-from test_sparse_ndarray import test_create_csr, test_create_row_sparse, test_sparse_nd_setitem
+from test_sparse_ndarray import test_create_csr, test_create_row_sparse
 
 set_default_context(mx.gpu(0))
 del test_support_vector_machine_l1_svm
