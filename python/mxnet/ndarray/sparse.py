f944122dbdfdaf26ce0c036d533dad4ca8d4628b
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-08-21 23:22:11 +0000

    add clarification for csr

--- python/mxnet/ndarray/sparse.py
+++ python/mxnet/ndarray/sparse.py
@@ -247,6 +247,9 @@ class CSRNDArray(BaseSparseNDArray):
     row i are stored in indices[indptr[i]:indptr[i+1]] and their corresponding values are stored
     in values[indptr[i]:indptr[i+1]].
 
+    The indices are expected to be sorted per row in ascending order. Duplicate matrix entries are
+    not allowed.
+
     Example
     -------
     >>> a = mx.nd.array([[0, 1, 0], [2, 0, 0], [0, 0, 0], [0, 0, 3]])
