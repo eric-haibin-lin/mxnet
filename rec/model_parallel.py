8a05139c402cfba8fff7e67e7c5bb4de02bd1ca5
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-10-31 06:02:09 +0000

    add allreduce

--- rec/model_parallel.py
+++ rec/model_parallel.py
@@ -2,21 +2,23 @@ import mxnet as mx
 import numpy as np
 from mxnet import nd, autograd, gluon
 
+mx.random.seed(0)
+np.random.seed(0)
+num_gpus = 1
+ctx_list = [mx.gpu(i) for i in range(num_gpus)]
+
 num_inputs = 240
 num_hidden = 512
 num_outputs = 240
-num_examples = 32
+num_examples = 320
+batch_size = 32
 
 def real_fn(X):
     return X
 
-num_gpus = 1
-ctx_list = [mx.gpu(0)]
-
 X = nd.random.normal(shape=(num_examples, num_inputs))
 y = X
 
-batch_size = 2
 train_data = mx.gluon.data.DataLoader(mx.gluon.data.ArrayDataset(X, y),
                                       batch_size=batch_size, shuffle=True)
 
@@ -32,7 +34,9 @@ w2 = gluon.utils.split_and_load(w2_cpu, ctx_list, batch_axis=1)
 b1 = [b1_cpu.copyto(ctx) for ctx in ctx_list]
 b2 = [b2_cpu.copyto(ctx) for ctx in ctx_list]
 
-params = w1 + w2 + b1 + b2
+shard_params = w1 + w2
+non_shard_params = b1 + b2
+params = shard_params + non_shard_params
 for param in params:
     param.attach_grad()
 
@@ -50,8 +54,12 @@ def net(Xs):
 def square_loss(yhat, y):
     return nd.mean((yhat - y) ** 2)
 
-def test_loss(yhat, y):
-    return nd.sum(yhat)
+def allreduce(data):
+    # sum on data[0].context, and then broadcast
+    for i in range(1, len(data)):
+        data[0][:] += data[i].copyto(data[0].context)
+    for i in range(1, len(data)):
+        data[0].copyto(data[i])
 
 def SGD(params, lr):
     for param in params:
@@ -71,17 +79,23 @@ for e in range(epochs):
         with autograd.record():
             outputs = net(datas)
             losses = [square_loss(output, label) for output, label in zip(outputs, labels)]
-        mx.nd.waitall()
         for l in losses:
             l.backward()
-        '''
-        #SGD(params, learning_rate)
+
+        #print(nd.sum(b1[0].grad))
+        #print(nd.sum(b1[1].grad))
+        # aggregate gradient over GPUs for b1 and b2
+        for p in range(len(non_shard_params)):
+            allreduce([non_shard_params[p][ctx].grad for ctx in range(num_gpus)])
+        # perform update
+        SGD(params, learning_rate)
 
         ##########################
         #  Keep a moving average of the losses
         ##########################
         niter += 1
-        curr_loss = nd.mean(loss).asscalar()
+        losses_cpu = [loss.copyto(mx.cpu()) for loss in losses]
+        curr_loss = mx.nd.mean(*losses_cpu).asscalar()
         moving_loss = (1 - smoothing_constant) * moving_loss + (smoothing_constant) * curr_loss
 
         # correct the bias from the moving averages
@@ -89,5 +103,3 @@ for e in range(epochs):
 
         if (i + 1) % 10 == 0:
             print("Epoch %s, batch %s. Moving avg of loss: %s" % (e, i, est_loss))
-        '''
-        mx.nd.waitall()
