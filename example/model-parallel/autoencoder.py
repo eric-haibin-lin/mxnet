f423208cc188217184afe1e3368b5145064b4292
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-11-21 05:56:36 +0000

    refactor w/ gpu devices

--- example/model-parallel/autoencoder.py
+++ example/model-parallel/autoencoder.py
@@ -16,62 +16,61 @@
 # under the License.
 
 import mxnet as mx
+import argparse
 import logging
+from model import *
 head = '%(asctime)-15s %(message)s'
 logging.basicConfig(level=logging.INFO, format=head)
 
-def linear_model(num_gpus, num_hidden, num_input):
-    assert(num_gpus == 2)
-    act_list = []
-    loss_list = []
-    for gpu in range(num_gpus):
-        with mx.AttrScope(ctx_group='gpu_' + str(gpu)):
-            data = mx.symbol.Variable('data_' + str(gpu))
-            fc1  = mx.symbol.FullyConnected(data=data, num_hidden=num_hidden/num_gpus)
-            act1 = mx.symbol.Activation(data=fc1, act_type="relu")
-            act_list.append(act1)
+parser = argparse.ArgumentParser(description="Run autoencoder with model parallelism on multi-GPU",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--num-gpus', type=int, default=2,
+                    help='number of gpus to use')
+parser.add_argument('--num-epochs', type=int, default=5,
+                    help='number of epochs to train')
+parser.add_argument('--batch-size', type=int, default=128,
+                    help='number of examples per data batch')
+parser.add_argument('--num-hidden', type=int, default=512,
+                    help='number of hidden units')
 
-    for gpu in range(num_gpus):
-        with mx.AttrScope(ctx_group='gpu_' + str(gpu)):
-            label = mx.symbol.var('label_' + str(gpu))
-            concat = mx.symbol.Concat(*act_list)
-            fc2 = mx.symbol.FullyConnected(concat, num_hidden=num_input/num_gpus)
-            loss = mx.symbol.LinearRegressionOutput(fc2, label)
-            loss_list.append(loss)
-    return mx.sym.Group(loss_list)
+args = parser.parse_args()
+logging.info(args)
 
-group2ctx = {
-    'gpu_0' : mx.cpu(0),
-    'gpu_1' : mx.cpu(1)
-}
+num_epoch = args.num_epochs
+num_hidden = args.num_hidden
+batch_size = args.batch_size
+num_gpus = args.num_gpus
+num_batches = 100
+num_input = 240000
 
-num_epoch = 5
-num_batches = 10
-num_input = 10
-num_hidden = 2
-batch_size = 8
-num_gpus = 2
-sym = linear_model(num_gpus, num_hidden, num_input)
+# model
+net = model(num_gpus, num_hidden, num_input)
+# parallel gpu groups
+group2ctx = {gpu_id(i) : mx.gpu(i) for i in range(num_gpus)}
 
+# dataset
+iters = []
 rand_data = mx.nd.ones((batch_size * num_batches, num_input))
 rand_labels = rand_data.split(axis=1, num_outputs=num_gpus)
-
-iters = []
 data_names = ['data_' + str(i) for i in range(num_gpus)]
 label_names = ['label_' + str(i) for i in range(num_gpus)]
+
 for i in range(num_gpus):
-    data_iter_i = mx.io.NDArrayIter({data_names[i]:rand_data}, {label_names[i]:rand_labels[i]},
-                                    batch_size=batch_size)
-    iters.append(data_iter_i)
+    iter_i = mx.io.NDArrayIter({data_names[i]:rand_data},
+                               {label_names[i]:rand_labels[i]},
+                               batch_size=batch_size)
+    iters.append(iter_i)
 train_data = mx.io.PrefetchingIter(iters)
 
-mod = mx.mod.Module(symbol=sym, data_names=data_names, label_names=label_names)
-mod.bind(data_shapes=train_data.provide_data, label_shapes=train_data.provide_label)
+# module
+mod = mx.mod.Module(symbol=net, data_names=data_names,
+                    label_names=label_names, group2ctxs=[group2ctx])
+mod.bind(data_shapes=train_data.provide_data,
+         label_shapes=train_data.provide_label)
 mod.init_params()
-optim = mx.optimizer.create('sgd', learning_rate=0.01)
-mod.init_optimizer(optimizer=optim)
+mod.init_optimizer(optimizer='adam')
 metric = mx.metric.create(['MSE'])
-speedometer = mx.callback.Speedometer(batch_size, 1)
+speedometer = mx.callback.Speedometer(batch_size, 10)
 
 logging.info('Training started ...')
 data_iter = iter(train_data)
@@ -81,9 +80,7 @@ for epoch in range(num_epoch):
     for batch in data_iter:
         nbatch += 1
         mod.forward_backward(batch)
-        # update all parameters (including the weight parameter)
         mod.update()
-        # update training metric
         mod.update_metric(metric, batch.label)
         speedometer_param = mx.model.BatchEndParam(epoch=epoch, nbatch=nbatch,
                                                    eval_metric=metric, locals=locals())
@@ -92,5 +89,3 @@ for epoch in range(num_epoch):
     score = mod.score(train_data, ['MSE'])
     logging.info('epoch %d, eval metric = %s ' % (epoch, score[0][1]))
     data_iter.reset()
-
-mx.nd.waitall()
